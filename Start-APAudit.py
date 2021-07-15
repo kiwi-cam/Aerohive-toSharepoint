78ed42eb8bbda4185c7cae52c6d91965102bbd9c
Cameron McConnochie <32912464+kiwi-cam@users.noreply.github.com>
2021-07-15 20:32:43 +1200

    Merged AP info into single write to SP

--- Start-APAudit.py
+++ Start-APAudit.py
@@ -9,7 +9,6 @@ import re
 import sys
 import warnings
 import pprint
-import tabulate
 import os
 from office365.runtime.auth.authentication_context import AuthenticationContext
 from office365.sharepoint.client_context import ClientContext
@@ -81,108 +80,64 @@ def get_info(AP):
     access_point = aeromiko.AP(AP['IPAddress'], AP['Username'], AP['Password'])
     access_point.connect()
 
-    ap_hostname(access_point)
-    ap_cpu(access_point)
-    ap_channels(access_point)
-    ap_neighbors(access_point)
-    ap_stations(access_point)
-    ap_radios(access_point)
+    # get hostname via Aeromiko
+    access_point.hostname = access_point.get_hostname()
+    # get model# and uptime info via Aeromiko
+    version_info = access_point.show_version()
+    access_point.platform = version_info["PLATFORM"]
+    access_point.uptime = version_info["UPTIME"]
+    # get lldp information via Aeromiko
+    #lldp_info = access_point.show_lldp_neighbor()
+    #access_point.lldp_neighbor = lldp_info["SYSTEM_NAME"]
+    #access_point.lldp_neighbor_port = lldp_info["PORT_DESC"]
 
-    #     #                      #
-    #     #                      #
-    #     ####    ###    ####  #####  # ##    ####  ## #    ###
-    #     #   #  #   #  #        #    ##  #  #   #  # # #  #   #
-    #     #   #  #   #   ###     #    #   #  #   #  # # #  #####
-    #     #   #  #   #      #    #    #   #  #  ##  # # #  #
-    #     #   #   ###   ####      ##  #   #   ## #  #   #   ###
+    # get eth0 information via Aeromiko
+    eth0_info = access_point.show_int_eth("eth0")
+    access_point.link_duplex = eth0_info["DUPLEX"]
+    access_point.link_speed = eth0_info["SPEED"]
 
-
-def ap_hostname(access_point):
-    try:
-        # get hostname via Aeromiko
-        access_point.hostname = access_point.get_hostname()
-        # print hostname with Figlet and plain text
-        figlet = Figlet(font="big")
-        if not silent: print(colorize(figlet.renderText(access_point.hostname), Cyan))
-        if not silent: print(colorize(access_point.hostname, Cyan))
-
-        # get model# and uptime info via Aeromiko
-        version_info = access_point.show_version()
-        access_point.platform = version_info["PLATFORM"]
-        access_point.uptime = version_info["UPTIME"]
-        # format uptime for display
-        # input :  1 weeks, 3 days, 6 hours, 45 minutes, 33 seconds
-        # output: 1w 3d 6h 45m 33s
-        #
-        # remove commas
-        access_point.uptime = re.sub(",", "", access_point.uptime)
-        # keep only the first letter of each word
-        access_point.uptime = re.sub("([a-z])[a-z]+", r"\1", access_point.uptime)
-        # remove spaces immediately following numbers
-        access_point.uptime = re.sub(r"(\d+) ", r"\1", access_point.uptime)
-        # print model number and uptime
-        sharepoint_update('AccessPoints', {"AP_Name": access_point.hostname, "Model": access_point.platform, "Uptime": access_point.uptime}, "<Where><Eq><FieldRef Name='AP_Name' /><Value Type='Text'>"+access_point.hostname+"</Value></Eq></Where>")
-        if not silent: print("\nModel: " + colorize(access_point.platform, Cyan))
-        if not silent: print("Uptime: " + colorize(access_point.uptime, Cyan))
-
-        # get lldp information via Aeromiko
-        lldp_info = access_point.show_lldp_neighbor()
-        access_point.lldp_neighbor = lldp_info["SYSTEM_NAME"]
-        access_point.lldp_neighbor_port = lldp_info["PORT_DESC"]
-        # print port and system descriptions
-        if not silent:
-            print(
-                "\nport "
-                + colorize(access_point.lldp_neighbor_port, Magenta)
-                + " on "
-                + colorize(access_point.lldp_neighbor, Magenta)
-            )
-
-        # get eth0 information via Aeromiko
-        eth0_info = access_point.show_int_eth("eth0")
-        access_point.link_duplex = eth0_info["DUPLEX"]
-        access_point.link_speed = eth0_info["SPEED"]
-        # print link speed and duplex
-        if not silent:
-            print(
-                colorize(access_point.link_speed, Magenta)
-                + ", "
-                + colorize(access_point.link_duplex, Magenta)
-                + "\n\n"
-            )
-    except IndexError:
-        pass
-
-    #      ###   ####   #   #
-    #     #   #  #   #  #   #
-    #     #      #   #  #   #
-    #     #      ####   #   #
-    #     #      #      #   #
-    #     #   #  #      #   #
-    #      ###   #       ###
-
-
-def ap_cpu(access_point):
+    # get CPU Info via Aeromiko
     snapshot_cpu = access_point.show_cpu()
-
-    if not silent: print("Snapshot CPU Util")
-
     access_point.cpu_total = snapshot_cpu["CPU_TOTAL"]
-    if float(access_point.cpu_total) > 75:
-        access_point.cpu_total = colorize(access_point.cpu_total, Red)
-    elif float(access_point.cpu_total) > 50:
-        access_point.cpu_total = colorize(access_point.cpu_total, Yellow)
     access_point.cpu_system = snapshot_cpu["CPU_SYSTEM"]
     access_point.cpu_user = snapshot_cpu["CPU_USER"]
 
-    cpu_table = [["Total", access_point.cpu_total]]
-    cpu_table.append(["System", access_point.cpu_system])
-    cpu_table.append(["User", access_point.cpu_user])
-    
-    sharepoint_update('AccessPoints', {"AP_Name": access_point.hostname, "CPU_Total": access_point.cpu_total, "CPU_User": access_point.cpu_user, "CPU_System": access_point.cpu_system}, "<Where><Eq><FieldRef Name='AP_Name' /><Value Type='Text'>"+access_point.hostname+"</Value></Eq></Where>")
+    # Get Channel information 
+    access_point = ap_channels(access_point)
+    # get Radio information
+    access_point = ap_radios(access_point)
 
-    if not silent: print(tabulate.tabulate(cpu_table, tablefmt="psql"))
-    if not silent: print("\n")
+    sharepoint_update('AccessPoints', 
+         {
+             "AP_Name": access_point.hostname, 
+             "Model": access_point.platform,
+             "Uptime": access_point.uptime,
+             "CPU_Total": access_point.cpu_total,
+             "CPU_User": access_point.cpu_user,
+             "CPU_System": access_point.cpu_system,
+             "Wifi0_Channel": access_point.wifi0_Channel,	
+             "Wifi0_TX_Power": access_point.wifi0_TX_Power,
+             "Wifi1_Channel": access_point.wifi1_Channel,
+             "Wifi1_TX_Power": access_point.wifi1_TX_Power,
+             "wifi0_RX_Bytes": access_point.wifi0_rx_bytes,	
+             "wifi0_TX_Bytes": access_point.wifi0_tx_bytes,
+             "wifi1_RX_Bytes": access_point.wifi1_rx_bytes,
+             "wifi1_TX_Bytes": access_point.wifi1_tx_bytes,
+             "wifi0_RX_Drops": access_point.wifi0_RX_Drops,
+             "wifi0_TX_Drops": access_point.wifi0_TX_Drops,
+             "wifi1_RX_Drops": access_point.wifi1_RX_Drops,
+             "wifi1_TX_Drops": access_point.wifi1_TX_Drops,
+             "wifi0_RX_Errors": access_point.wifi0_RX_Errors,
+             "wifi0_TX_Errors": access_point.wifi0_TX_Errors,
+             "wifi1_RX_Errors": access_point.wifi1_RX_Errors,
+             "wifi1_TX_Errors": access_point.wifi1_TX_Errors
+         }, 
+         "<Where><Eq><FieldRef Name='AP_Name' /><Value Type='Text'>"+access_point.hostname+"</Value></Eq></Where>"
+    )
+
+    ap_neighbors(access_point)
+    ap_stations(access_point)
+
 
     #            #                                   ##
     #            #                                    #
@@ -204,8 +159,6 @@ def ap_channels(access_point):
             key = key.lower()
             setattr(access_point, key, value)
 
-    channels_table = [["AP Name", "Int", "CH", "TX Power"]]
-
     for radio in ("wifi0", "wifi1"):
         # use getattr cause string + variable concat getting used as var name
         channel_select_state = getattr(
@@ -219,20 +172,12 @@ def ap_channels(access_point):
                 man_chan = getattr(access_point, primary_channel) + "*"
                 setattr(access_point, primary_channel, man_chan)
 
-        color = Yellow
-        if radio == "wifi1":
-            color = Blue
-
-        # use getattr cause string + variable concat getting used as var name
-        interface = colorize(getattr(access_point, radio + "_interface"), color)
-        chan = colorize(getattr(access_point, primary_channel), color)
-        txpower = colorize(getattr(access_point, radio + "_tx_power_dbm"), color)
-
-        channels_table.append([access_point.hostname, interface, chan, txpower])
-        sharepoint_update('AccessPoints', {"AP_Name": access_point.hostname, getattr(access_point, radio + "_interface")+"_Channel": getattr(access_point, primary_channel), getattr(access_point, radio + "_interface")+"_TX_Power": getattr(access_point, radio + "_tx_power_dbm")}, "<Where><Eq><FieldRef Name='AP_Name' /><Value Type='Text'>"+access_point.hostname+"</Value></Eq></Where>")
-
-    if not silent: print(tabulate.tabulate(channels_table, headers="firstrow", tablefmt="psql"))
-    if not silent: print("* denotes manual setting")
+        interface = getattr(access_point, radio + "_interface")
+        chan = getattr(access_point, primary_channel)
+        txpower = getattr(access_point, radio + "_tx_power_dbm")
+        setattr(access_point, radio + "_Channel", getattr(access_point, primary_channel)) 
+        setattr(access_point, radio + "_TX_Power", getattr(access_point, radio + "_tx_power_dbm"))
+    return access_point
 
     #                    #           #      #
     #                                #      #
@@ -256,7 +201,6 @@ def ap_neighbors(access_point):
         parsed_neighbors, key=operator.itemgetter("CHANNEL")
     )
 
-    if not silent: print("\n\nACSP Neighbors >= -85dBm")
     for neighbor in parsed_neighbors:
         # if BBSID !unique, drop it
         if neighbor["BSSID"] not in bssid_list:
@@ -264,7 +208,6 @@ def ap_neighbors(access_point):
 
             # only show neighbors that we have to share airtime with
             if int(neighbor["RSSI"]) >= -85:
-                
                 neighbor["AP_Name"] = access_point.hostname
 
                 # copy radio channel so we can decorate the copy and still
@@ -277,31 +220,11 @@ def ap_neighbors(access_point):
                         " (" + neighbor["CHANNEL_WIDTH"] + "MHz)"
                     )
 
-                # columns to be output into table
-                print_columns = [
-                    neighbor["AP_Name"],
-                    neighbor["BSSID"],
-                    neighbor["DECORATED_CHANNEL"],
-                    neighbor["RSSI"],
-                    neighbor["SSID"],
-                    neighbor["CU"],
-                    neighbor["CRC"],
-                    neighbor["STA"],
-                ]
-
                 w0_channel = re.sub("[*]", "", access_point.wifi0_primary_channel)
                 w1_channel = re.sub("[*]", "", access_point.wifi1_primary_channel)
 
-                if neighbor["CHANNEL"] == w0_channel:
-                    print_columns = column_colorize(print_columns, Yellow)
-                elif neighbor["CHANNEL"] == w1_channel:
-                    print_columns = column_colorize(print_columns, Blue)
-
-                neighbor_table.append(print_columns)
                 sharepoint_update('Neighbors', neighbor, "<Where><And><Eq><FieldRef Name='AP_Name' /><Value Type='Text'>"+neighbor["AP_Name"]+"</Value></Eq><Eq><FieldRef Name='BSSID' /><Value Type='Text'>"+neighbor["BSSID"]+"</Value></Eq></And></Where>")
 
-    if not silent: print(tabulate.tabulate(neighbor_table, headers="firstrow", tablefmt="psql"))
-    if not silent: print("\n")
 
     #              #             #      #
     #              #             #
@@ -314,7 +237,6 @@ def ap_neighbors(access_point):
 
 def ap_stations(access_point):
     parsed_stations = access_point.show_station()
-    if not silent: print("Stations on this AP")
     station_table = [
         [
             "AP Name",
@@ -340,38 +262,15 @@ def ap_stations(access_point):
     for station in parsed_stations:
         station["AP_Name"] = access_point.hostname
         if station["IP_ADDR"] == "0.0.0.0":
-            station["IP_ADDR"] = colorize(station["IP_ADDR"], Red)
             station["Hostname"] = ""
         else:
             station["Hostname"] = socket.gethostbyaddr(station["IP_ADDR"])[0]
 
-        print_columns = [
-            station["AP_Name"],
-            station["SSID"],
-            station["MAC_ADDR"],
-            station["IP_ADDR"],
-            station["Hostname"],
-            station["CHAN"],
-            station["TX_RATE"],
-            station["RX_RATE"],
-            station["POW_SNR"],
-            station["ASSOC_TIME"],
-            station["PHYMODE"],
-            station["STATION_STATE"],
-        ]
-
         w0_channel = re.sub("[*]", "", access_point.wifi0_primary_channel)
         w1_channel = re.sub("[*]", "", access_point.wifi1_primary_channel)
 
-        if station["CHAN"] == w0_channel:
-            print_columns = column_colorize(print_columns, Yellow)
-        elif station["CHAN"] == w1_channel:
-            print_columns = column_colorize(print_columns, Blue)
-
         sharepoint_update('Stations', station, "<Where><Eq><FieldRef Name='MAC_ADDR' /><Value Type='Text'>"+station["MAC_ADDR"]+"</Value></Eq></Where>")
-        station_table.append(print_columns)
 
-    if not silent: print(tabulate.tabulate(station_table, headers="firstrow", tablefmt="psql"))
 
     #                       #    #
     #                       #
@@ -384,12 +283,8 @@ def ap_stations(access_point):
 
 def ap_radios(access_point):
     for radio in ("wifi0", "wifi1"):
-
-        color = Yellow
-        if radio == "wifi1":
-            color = Blue
-
         parsed_radio_int = access_point.show_int_wifi(radio)
+
         # prepend wifiN_ before conversion to  properties
         for (key, value) in parsed_radio_int[0].items():
             key = radio + "_" + key
@@ -405,83 +300,12 @@ def ap_radios(access_point):
             stringified = str(rounded) + "%"
             return stringified
 
-        # build a list for tabulate to print as a table
-        data_table = [
-            [colorize(radio, color), "", "", ""],
-            ["", "Rx", "Tx", ""],
-            ["Bytes", AP_info("rx_bytes"), AP_info("tx_bytes"), "", ""],
-            [
-                "Drops",
-                percent("rx_drops", "rx_packets"),
-                percent("tx_drops", "tx_packets"),
-            ],
-            [
-                "Errors",
-                percent("rx_err", "rx_packets"),
-                percent("tx_err", "tx_packets"),
-            ],
-            [],
-            {"Utilization"},
-            ["Rx", "Tx", "Interference", "Total"],
-            [
-                AP_info("rx_util"),
-                AP_info("tx_util"),
-                AP_info("interference_util"),
-                AP_info("total_util"),
-            ],
-            [],
-            ["", "Snapshot", "Short Avg", "Running Avg"],
-            [
-                "Rx CU",
-                AP_info("snap_rx_cu"),
-                AP_info("stma_rx_cu"),
-                AP_info("run_avg_rx_cu"),
-                "",
-            ],
-            [
-                "Tx CU",
-                AP_info("snap_tx_cu"),
-                AP_info("stma_tx_cu"),
-                AP_info("run_avg_tx_cu"),
-                "",
-            ],
-            [
-                "Interference",
-                AP_info("snap_interference_cu"),
-                AP_info("stma_interference_cu"),
-                AP_info("run_avg_interference_cu"),
-                "",
-            ],
-            [
-                "Noise",
-                AP_info("snap_noise"),
-                AP_info("stma_noise"),
-                AP_info("run_avg_noise"),
-                "",
-            ],
-        ]
-        sharepoint_update('AccessPoints', {"AP_Name": access_point.hostname, radio+"_RX_Bytes": AP_info("rx_bytes"), radio+"_TX_Bytes": AP_info("tx_bytes"), radio+"_RX_Drops": percent("rx_drops", "rx_packets"), radio+"_TX_Drops": percent("tx_drops", "tx_packets"), radio+"_RX_Errors": percent("rx_err", "rx_packets"), radio+"_TX_Errors": percent("rx_err", "rx_packets")}, "<Where><Eq><FieldRef Name='AP_Name' /><Value Type='Text'>"+access_point.hostname+"</Value></Eq></Where>")
-        if not silent: print("\n" + tabulate.tabulate(data_table, headers="firstrow", tablefmt="psql"))
+        setattr(access_point, radio+"_RX_Drops", percent("rx_drops", "rx_packets")) 
+        setattr(access_point, radio+"_TX_Drops", percent("tx_drops", "tx_packets")) 
+        setattr(access_point, radio+"_RX_Errors", percent("rx_err", "rx_packets"))
+        setattr(access_point, radio+"_TX_Errors", percent("tx_err", "tx_packets"))
 
-
-#                    ##                    #
-#                     #
-#      ####   ###     #     ###   # ##    ##    #####   ###
-#     #      #   #    #    #   #  ##       #       #   #   #
-#     #      #   #    #    #   #  #        #      #    #####
-#     #      #   #    #    #   #  #        #     #     #
-#      ####   ###    ###    ###   #       ###   #####   ###
-
-
-Black = "\u001b[30;1m"  # Black
-Red = "\u001b[31;1m"  # Red
-Green = "\u001b[32;1m"  # Green
-Yellow = "\u001b[33;1m"  # Yellow
-Blue = "\u001b[34;1m"  # Blue
-Magenta = "\u001b[35;1m"  # Magenta
-Cyan = "\u001b[36;1m"  # Cyan
-White = "\u001b[37;1m"  # White
-End = "\033[0m"
+    return access_point
 
 OPERATOR_SYMBOLS = {
     "<": operator.lt,
@@ -493,17 +317,6 @@ OPERATOR_SYMBOLS = {
     "in": operator.contains,
 }
 
-
-def colorize(what_to_color, color):
-    return color + what_to_color + End
-
-
-def column_colorize(print_columns, color):
-    new_columns = []
-    for column in print_columns:
-        new_column = colorize(column, color)
-        new_columns.append(new_column)
-    return new_columns
 
 def sharepoint_update(listName, item, dupQuery):
     SPlist = ctx.web.lists.get_by_title(listName)
